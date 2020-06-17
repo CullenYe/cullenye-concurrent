16e005e899b5d950f77e749596a1dc80bb90e8a5
CullenYe <549651517@qq.com>
2020-06-18 01:15:14 +0800

    CountDownLlatch,CyclicBarrier

---
+++ src/main/java/com/cullenye/concurrent/ch2/UseCountDownLatch.java
@@ -0,0 +1,73 @@
+package com.cullenye.concurrent.ch2;
+
+import java.util.concurrent.CountDownLatch;
+
+public class UseCountDownLatch {
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(6);
+
+    //初始化线程
+    private static class InitialThread implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("初始化线程Thread_"+Thread.currentThread().getId() +"准备开始初始化工作");
+            //初始化工作内容......
+
+            countDownLatch.countDown();
+
+            //初始化完成后的其它工作......
+            System.out.println("初始化线程Thread_"+Thread.currentThread().getId() +"已完成初始化后的其它工作");
+        }
+
+    }
+
+    //业务线程，需要等待初始化线程完成后才进行工作
+    private static class BusinessThread implements Runnable{
+        @Override
+        public void run() {
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("业务线程"+Thread.currentThread().getId() +"继续完成工作");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // 开启单独的初始化线程，分2步初始化，countDownLatch需要扣减2次
+        new Thread(() -> {
+            System.out.println("单独的初始化线程开始初始化工作");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            countDownLatch.countDown();
+            System.out.println("单独的初始化线程完成了第1步初始化工作");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            countDownLatch.countDown();
+            System.out.println("单独的初始化线程完成了第2步初始化工作");
+        }).start();
+
+        //开启业务线程
+        new Thread(new BusinessThread()).start();
+
+        //开启4个初始化线程
+        for(int i=0;i<=3;i++)
+        {
+            new Thread(new InitialThread()).start();
+        }
+
+        //主线程等待初始化工作完成
+        countDownLatch.await();
+
+        System.out.println("主线程等待初始化完成后开始进行其它工作");
+    }
+}
---
+++ src/main/java/com/cullenye/concurrent/ch2/UseCyclicBarrier.java
@@ -0,0 +1,62 @@
+package com.cullenye.concurrent.ch2;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CyclicBarrier;
+
+public class UseCyclicBarrier {
+
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(5,new CollectThread());
+
+    //存放子线程工作结果的容器
+    private static ConcurrentHashMap<String,Long> resultMap = new ConcurrentHashMap<>();
+
+    //工作子线程
+    private static class SubThread implements Runnable{
+
+        @Override
+        public void run() {
+            //线程本身的工作结果
+            long id = Thread.currentThread().getId();
+            resultMap.put(Thread.currentThread().getId()+"",id);
+
+            //随机决定工作线程是否睡眠
+            Random r = new Random();
+            try {
+                if(r.nextBoolean()) {
+                    Thread.sleep(2000+id);
+                }
+                System.out.println("线程"+id+"到达屏障，正在等待");
+                cyclicBarrier.await();
+                Thread.sleep(1000+id);
+                System.out.println("线程"+id+"在屏障开发后完成业务操作");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    //屏障开放后执行的线程，将容器中所有工作子线程的工作结果进行字符串拼接，并输出
+    private static class CollectThread implements Runnable{
+
+        @Override
+        public void run() {
+            StringBuilder result = new StringBuilder();
+            for(Map.Entry<String,Long> subThreadResult:resultMap.entrySet()){
+                result.append("["+subThreadResult.getValue()+"]");
+            }
+            System.out.println("CollectThread线程取得的所有子线程工作结果： " + result);
+            System.out.println("CollectThread线程完成其它工作");
+        }
+    }
+
+    public static void main(String[] args) {
+        //启动5个工作子线程
+        for(int i=0;i<5;i++){
+            Thread thread = new Thread(new SubThread());
+            thread.start();
+        }
+    }
+}
