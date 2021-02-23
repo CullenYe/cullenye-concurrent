71231d6756987c20836b06e2fd0c040631037241
CullenYe <549651517@qq.com>
2021-02-24 00:10:24 +0800

    实现自定义线程池和测试

--- src/main/java/com/cullenye/concurrent/ch6pool/MyThreadPool.java
+++ src/main/java/com/cullenye/concurrent/ch6pool/MyThreadPool.java
@@ -32,9 +32,8 @@ public class MyThreadPool {
     }
 
     public MyThreadPool(int task_count,int work_number){
-        if(work_number < 0){
-            work_number = WORK_COUNT;
-        }
+        if(work_number <= 0) work_number = WORK_COUNT;
+        if(task_count <= 0) task_count = 100;
         this.taskQueue = new ArrayBlockingQueue<>(task_count);
         this.work_number = work_number;
         workThreads = new WorkThread[work_number];
@@ -45,6 +44,20 @@ public class MyThreadPool {
         }
     }
 
+    /**
+     * 放入任务，但只是加入队列
+     */
+    public void execute(Runnable task){
+        try {
+            taskQueue.put(task);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 销毁线程池
+     */
     public void destroy(){
         System.out.println("准备关闭线程池");
         for(int i=0;i<workThreads.length;i++){
@@ -54,6 +67,12 @@ public class MyThreadPool {
         }
         // 清空任务队列
         taskQueue.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "WorkThread number:"+work_number
+                +" wait task number:"+taskQueue.size();
     }
 
     /**
@@ -67,30 +86,20 @@ public class MyThreadPool {
                 try {
                     r = taskQueue.take();
                     if(r != null){
-                        System.out.println(getId() + "准备执行");
+                        System.out.println(getId() + "准备执行" + ((TestMyThreadPool.MyTask)r).getName());
                         r.run();
                     }
                     // 执行完后手动置为空
                     r = null;
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    // 因为没有任务时taskQueue.take()会阻塞，此时如果销毁线程池，关闭工作线程，会抛异常，所以不打印异常
+                    //e.printStackTrace();
                 }
             }
         }
 
         /**
-         * 放入任务，但只是加入队列
-         */
-        public void execute(Runnable task){
-            try {
-                taskQueue.put(task);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        /**
-         * 关闭线程池
+         * 停止工作线程
          */
         public void stopWorker() {
             interrupt();
---
+++ src/main/java/com/cullenye/concurrent/ch6pool/TestMyThreadPool.java
@@ -0,0 +1,49 @@
+package com.cullenye.concurrent.ch6pool;
+
+import java.util.Random;
+
+public class TestMyThreadPool {
+
+    public static void main(String[] args) throws InterruptedException {
+        MyThreadPool myThreadPool = new MyThreadPool(0,3);
+        myThreadPool.execute(new MyTask("testA"));
+        myThreadPool.execute(new MyTask("testB"));
+        myThreadPool.execute(new MyTask("testC"));
+        myThreadPool.execute(new MyTask("testD"));
+        myThreadPool.execute(new MyTask("testE"));
+        System.out.println(myThreadPool);
+        Thread.sleep(10000);
+        // 所有线程都执行完成才destory
+        myThreadPool.destroy();
+        System.out.println(myThreadPool);
+    }
+
+    /**
+     * 任务类
+     */
+    static class MyTask implements Runnable{
+
+        private String name;
+        private Random r = new Random();
+
+        public MyTask(String name){
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(r.nextInt(1000) + 2000);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getId() + " sleep InterruptedException:"
+                    +Thread.currentThread().isInterrupted());
+                e.printStackTrace();
+            }
+            System.out.println("任务 " + name + " 完成");
+        }
+    }
+}
