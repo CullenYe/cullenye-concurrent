f458a5c4041b461e7627a5295818c4435bc9f3cf
cullenye <549651517@qq.com>
2021-03-30 19:17:42 +0800

    添加简单的死锁

--- src/main/java/com/cullenye/concurrent/ch7safe/NormalDeadLock.java
+++ src/main/java/com/cullenye/concurrent/ch7safe/NormalDeadLock.java
@@ -12,4 +12,58 @@ public class NormalDeadLock {
      * 第二个锁
      */
     private static Object valueSecond = new Object();
+
+    // 先拿第一个锁，再拿第二个锁
+    private static void fisrtToSecond() throws InterruptedException{
+        String threadName = Thread.currentThread().getName();
+        synchronized (valueFirst){
+            System.out.println(threadName+" get first");
+            Thread.sleep(100);
+            synchronized (valueSecond){
+                System.out.println(threadName+" get second");
+            }
+        }
+    }
+
+    // 先拿第二个锁，再拿第一个锁
+    private static void SecondToFisrt() throws InterruptedException {
+        String threadName = Thread.currentThread().getName();
+        synchronized (valueSecond){
+            System.out.println(threadName+" get second");
+            Thread.sleep(100);
+            synchronized (valueFirst){
+                System.out.println(threadName+" get first");
+            }
+        }
+    }
+
+    private static class TestThread extends Thread{
+
+        private String name;
+
+        public TestThread(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            Thread.currentThread().setName(name);
+            try {
+                SecondToFisrt();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread.currentThread().setName("主线程");
+        TestThread testThread = new TestThread("子线程");
+        testThread.start();
+        try {
+            fisrtToSecond();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
